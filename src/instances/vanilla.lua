05fd80cde4ec12dacb9fb6babed0c8d1f9ee41d8
Pontus Nilsson <me@nilssonpontus.com>
2024-02-24 17:22:05 +0100

    Override vanilla dungeons for SoD
    Moves BFD & Gnomeregan to Raids

--- src/instances/vanilla.lua
+++ src/instances/vanilla.lua
@@ -19,8 +19,6 @@ local Dungeons = {
 		"DM",
 		"SFK",
 		"STOCKS",
-		"BFD",
-		"GNOMER",
 		"RFK",
 		-- "SM",
 		"SM_GY",
@@ -48,10 +46,8 @@ local Dungeons = {
 		DM 				= "The Deadmines",
 		SFK 			= "Shadowfang Keep",
 		STOCKS 			= "The Stockade",
-		BFD 			= "Blackfathom Deeps",
-		GNOMER 			= "Gnomeregan",
 		RFK 			= "Razorfen Kraul",
-		-- SM 				= "Scarlet Monastery: Graveyard",
+		-- SM
 		SM_GY 			= "Scarlet Monastery: Graveyard",
 		SM_LIB 			= "Scarlet Monastery: Library",
 		SM_ARM 			= "Scarlet Monastery: Armory",
@@ -77,10 +73,8 @@ local Dungeons = {
 		DM				= { 18, 23, },
 		SFK				= { 22, 30, },
 		STOCKS			= { 22, 30, },
-		BFD				= { 24, 32, },
-		GNOMER			= { 29, 38, },
 		RFK				= { 30, 40, },
-		-- SM 				= { 28, 45, },
+		-- SM
 		SM_GY			= { 28, 38, },
 		SM_LIB			= { 29, 39, },
 		SM_ARM			= { 32, 42, },
@@ -95,7 +89,7 @@ local Dungeons = {
 		UBRS			= { 58, 60, },
 		SCHOLO			= { 58, 60, },
 		STRAT			= { 58, 60, },
-		-- DIRE_MAUL		= { 58, 60, },
+		-- DIRE_MAUL
 		DIRE_MAUL_E		= { 58, 60, },
 		DIRE_MAUL_N		= { 58, 60, },
 		DIRE_MAUL_W		= { 58, 60, },
@@ -106,10 +100,8 @@ local Dungeons = {
 		DM				= { "deadmines", "vc", "vancleef", "mine", "mines", },
 		SFK				= { "sfk", "shadowfang", },
 		STOCKS			= { "stk", "stock", "stockade", "stockades" },
-		BFD				= { "bfd", "fathom", "blackfathom", },
-		GNOMER			= { "gno", "gnom", "gnomeregan", "gnomeragan", "gnome", "gnomregan", "gnomragan", "gnomer" },
 		RFK				= { "rfk", "kraul", },
-		-- SM 				= { "sm", "scarlet", "mona", "monastery", },
+		-- SM
 		SM_GY			= { "smgy", "smg", "gy", "graveyard", },
 		SM_LIB			= { "smlib", "sml", "lib", "library", },
 		SM_ARM			= { "smarm", "sma", "arm", "armory", "herod", "armoury", "arms", },
@@ -124,11 +116,48 @@ local Dungeons = {
 		UBRS			= { "ubrs", "urbs", "rend", },
 		SCHOLO			= { "scholomance", "scholo", "sholo", "sholomance", },
 		STRAT			= { "stratlive", "live", "living", "stratud", "undead", "ud", "baron", "stratholme", "stath", "stratholm", "strah", "strath", "strat", "starth" },
-		-- DIRE_MAUL		= { "dire", "maul", "diremaul", },
+		-- DIRE_MAUL
 		DIRE_MAUL_E		= { "dme", "dmeast", "east", "puzilin", "jumprun", },
 		DIRE_MAUL_N		= { "dmn", "dmnorth", "north", "tribute", },
 		DIRE_MAUL_W		= { "dmw", "dmwest", "west", },
 	}
 }
 
+local Raids = {
+	Order = {
+	},
+	Names = {
+	},
+	Levels = {
+	},
+	Tags = {
+	}
+}
+
+if Utils.game.compareGameVersion(Utils.game.GameVersionLookup.SeasonOfDiscovery) then
+	-- BFD
+	Raids.Order[#Raids.Order + 1] = "BFD"
+	Raids.Names.BFD = "Blackfathom Deeps"
+	Raids.Levels.BFD = { 25, 35, }
+	Raids.Tags.BFD = { "bfd", "fathom", "blackfathom", }
+
+	-- Gnomeregan
+	Raids.Order[#Raids.Order + 1] = "GNOMER"
+	Raids.Names.GNOMER = "Gnomeregan"
+	Raids.Levels.GNOMER = { 40, 40, }
+	Raids.Tags.GNOMER = { "gno", "gnom", "gnomeregan", "gnomeragan", "gnome", "gnomregan", "gnomragan", "gnomer" }
+
+else
+	table.insert(Dungeons.Order, 6, "BFD")
+	Dungeons.Names.BFD = "Blackfathom Deeps"
+	Dungeons.Levels.BFD = { 24, 32, }
+	Dungeons.Tags.BFD = { "bfd", "fathom", "blackfathom", }
+
+	table.insert(Dungeons.Order, 7, "GNOMER")
+	Dungeons.Names.GNOMER = "Gnomeregan"
+	Dungeons.Levels.GNOMER = { 29, 38, }
+	Dungeons.Tags.GNOMER = { "gno", "gnom", "gnomeregan", "gnomeragan", "gnome", "gnomregan", "gnomragan", "gnomer" }
+end
+
 LFGAnnouncements.Instances.Register("DUNGEONS", Utils.game.GameExpansionLookup.Vanilla, Dungeons)
+LFGAnnouncements.Instances.Register("RAIDS", Utils.game.GameExpansionLookup.Vanilla, Raids)
